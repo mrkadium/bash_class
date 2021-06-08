82e21a3a3f77bec2c1b458dad69f11a304086e12
Mario Olivo <mario_rivera.olivo@outlook.es>
2021-06-08 16:01:51 -0600

    First commit
    Added firts class's file: primera_clase.sh

---
+++ primera_clase.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+bar=1234
+robar=5678
+
+# para hacer solo lectura
+readonly robar
+echo 'Read-only bar:'$robar
+
+# para quitar valor
+echo 'bar: '$bar
+unset bar
+echo 'Unset for $bar:' $bar
+
+# variables especiales
+# PID (Process ID)
+echo 'PID:' $$
+
+# nombre del script
+echo 'Filename:' $0
+
+# argumentos
+# $n (n = numero cualquiera del argumento)
+echo 'First argument:' $1
+echo 'Second argument:' $2
+
+# numero de argumentos
+echo 'Argument count:' $#
+
+# codigo de status del ultimo script,
+# como codigos de salida
+# $?
